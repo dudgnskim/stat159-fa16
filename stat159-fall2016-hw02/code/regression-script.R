e1ac32358b8e01b86509a819d86d8c0ee0b112fe
dudgnskim <dudgnskim@berkeley.edu>
2016-10-07 10:15:15 -0700

    finished

--- stat159-fall2016-hw02/code/regression-script.R
+++ stat159-fall2016-hw02/code/regression-script.R
@@ -11,10 +11,10 @@ save(reg_sum, file='data/regression.RData')
 
 #Generate scatterplot-tv-sales
 png('images/scatterplot-tv-sales.png')
-plot(tv, sales, pch=19, cex=1, col='tomato', main='Regression Analysis of Sales on TV')
+plot(tv, sales, pch=19, cex=1, col='lavender', main='Regression Analysis of Sales on TV')
 abline(reg)
 dev.off()
 pdf('images/scatterplot-tv-sales.pdf')
-plot(tv, sales, pch=19, cex=1, col='tomato', main='Regression Analysis of Sales on TV')
+plot(tv, sales, pch=19, cex=1, col='lavender', main='Regression Analysis of Sales on TV')
 abline(reg)
 dev.off()
