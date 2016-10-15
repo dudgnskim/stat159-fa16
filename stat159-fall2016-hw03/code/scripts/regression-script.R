07c1de1085e4d83d00a4e15ee809c5ebd350ff5c
dudgnskim <dudgnskim@berkeley.edu>
2016-10-14 20:16:48 -0700

    I think regression-script.R is done

--- stat159-fall2016-hw03/code/scripts/regression-script.R
+++ stat159-fall2016-hw03/code/scripts/regression-script.R
@@ -7,7 +7,7 @@
 
 setwd('~/coding/stat159/stat159-fa16/stat159-fall2016-hw03/')
 ## Read in Advertising.csv
-ad <- read.table('data/Advertising.csv', sep=',')
+ad <- read.table('data/Advertising.csv', sep=',', header = TRUE)
 sales <- ad$Sales
 tv <- ad$TV
 radio <- ad$Radio
@@ -15,21 +15,44 @@ news <- ad$Newspaper
 
 ## Scatterplots
 # scatterplot-tv-sales
-png('scatterplot-tv-sales.png')
+png('images/scatterplot-tv-sales.png')
 plot(sales ~ tv, cex=1, pch=19, col='tomato')
 dev.off()
 # scatterplot-radio-sales
-png('scatterplot-radio-sales.png')
+png('images/scatterplot-radio-sales.png')
 plot(sales ~ radio, cex=1, pch=19, col='navy')
 dev.off()
 # scatterplot-newspaper-sales
-png('scatterplot-newspaper-sales.png')
+png('images/scatterplot-newspaper-sales.png')
 plot(sales ~ news, cex=1, pch=19, col='orange')
 dev.off()
 
+## Regression of the predictors onto Sales
+# Simple regressions
+# sales ~ tv
+s_tv <- lm(sales ~ tv)
+st.sum <- summary(s_tv)
+
+# sales ~ radio
+s_radio <- lm(sales ~ radio)
+sr.sum <- summary(s_radio)
+
+# sales ~ newspaper
+s_news <- lm(sales ~ news)
+sn.sum <- summary(s_news)
+
+# Multivariate regression
+s_all <- lm(sales ~ tv+radio+news)
+sa.sum <- summary(s_all)
+
+## Residual plot
+res <- summary(s_all)$residuals
+png('residual-plot.png')
+plot(res, ylab='residuals')
+dev.off()
+
 ## Save regression.RData
-reg <- lm()
-save()
+save(sa.sum, st.sum, sr.sum, sn.sum, file='data/regression.RData')
 
 
 
