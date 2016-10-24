3bf15889d467fee3dfe63ef76017cd9896d051e2
dudgnskim <dudgnskim@berkeley.edu>
2016-10-24 16:43:46 -0700

    lab8

--- labs/lab8/app.R
+++ labs/lab8/app.R
@@ -6,17 +6,15 @@ palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
 #                          secret='zS9A15rTImWtgcPjq1B2Hz974Va+JSNTz3XjD6NJ')
 #rsconnect::deployApp('/Users/HOON/coding/stat159/stat159-fa16/labs/lab8')
 ### Import Advertising.csv and generate regression.RData, scatterplot-tv-sales.png
-ad <- read.csv('Advertising.csv', header=TRUE)
-tv <- ad$TV
-sales<- ad$Sales
+ad <- read.csv('Advertising.csv', header=TRUE)[,-1]
 
 ## Shiny
 ui <- fluidPage(
   headerPanel('Regression of Advertising data'),
   sidebarPanel(
-    selectInput('predictors', 'X variables', names(ad)[-c(1,5)]),
-    selectInput('response var.', 'Y variable', names(ad),
-                selected = names(ad)[5])
+    selectInput('predictors', 'X variables', names(ad)[-4]),
+    selectInput('response', 'Y variable', names(ad)[[4]],
+                selected = names(ad)[[4]])
   ),
   mainPanel(
     plotOutput('plot1')
@@ -24,25 +22,14 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  output$plot <- renderPlot(
-    title <- "Scatterplot of Sales on predictors",
-    plot(ad$Sales ~ input$variable, data=ad, pch=19, cex=1, col='blue')
-  )
+  selectedData <- reactive({
+    ad[, c(input$predictors, input$response)]
+  })
+  
+  output$plot1 <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(), pch = 20, cex = 2)
+  })
 }
 
 shinyApp(ui = ui, server = server)
-
-# Save regression.RData
-reg <- lm(sales ~ tv)
-reg_sum <- summary(reg)
-save(reg_sum, file='data/regression.RData')
-
-#Generate scatterplot-tv-sales
-png('images/scatterplot-tv-sales.png')
-plot(tv, sales, pch=19, cex=1, col='lavender', main='Regression Analysis of Sales on TV')
-abline(reg)
-dev.off()
-pdf('images/scatterplot-tv-sales.pdf')
-plot(tv, sales, pch=19, cex=1, col='lavender', main='Regression Analysis of Sales on TV')
-abline(reg)
-dev.off()
